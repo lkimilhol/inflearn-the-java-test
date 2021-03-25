a44dfac2f8bee72605d154fb5c87d37dc0206bc8
lkimilhol <lkimilhol@gmail.com>
2021-03-25 21:55:36 +0900

    확장 모델 구현

---
+++ src/test/java/me/lkimilhol/inflearnthejavatest/FindSlowTestExtension.java
@@ -0,0 +1,33 @@
+package me.lkimilhol.inflearnthejavatest;
+
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class FindSlowTestExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+    private static final long THRESHOLD = 1000l;
+
+    @Override
+    public void beforeTestExecution(ExtensionContext extensionContext) throws Exception {
+        ExtensionContext.Store store = getStore(extensionContext);
+        store.put("START_TIME", System.currentTimeMillis());
+    }
+
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
+        String testMethodName = extensionContext.getRequiredTestMethod().getName();
+        ExtensionContext.Store store = getStore(extensionContext);
+        long start_time = store.remove("START_TIME", long.class);
+        long duration = System.currentTimeMillis() - start_time;
+        if (duration > THRESHOLD) {
+            System.out.printf("please consider mark method [%s] with @SlowTest.\n", testMethodName);
+        }
+    }
+
+    private ExtensionContext.Store getStore(ExtensionContext context) {
+        String testClassName = context.getRequiredTestClass().getTypeName();
+        String testMethodName = context.getRequiredTestMethod().getName();
+        return context.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+    }
+
+}
